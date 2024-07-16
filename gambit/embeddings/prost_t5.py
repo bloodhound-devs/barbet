db1f78d5a727c36c4b3e037dbaf27c37daf8ff31
rturnbull <robert.turnbull@unimelb.edu.au>
2024-07-16 10:30:48 +1000

    :bug: fixing issue with prosttg embeddings due to the tokenizer

--- gambit/embeddings/prost_t5.py
+++ gambit/embeddings/prost_t5.py
@@ -18,14 +18,13 @@ class ProstT5Embedding(Embedding):
         # only GPUs support half-precision currently; if you want to run on CPU use full-precision (not recommended, much slower)
         self.model.full() if self.device=='cpu' else self.model.half()
 
-
     def embed(self, seq:str) -> torch.Tensor:
         """ Takes a protein sequence as a string and returns an embedding vector. """
-        
         # add pre-fixes accordingly (this already expects 3Di-sequences to be lower-case)
         # if you go from AAs to 3Di (or if you want to embed AAs), you need to prepend "<AA2fold>"
         # if you go from 3Di to AAs (or if you want to embed 3Di), you need to prepend "<fold2AA>"
-        batch = [f"<AA2fold> {seq}"]
+        spaced_seq = " ".join(seq.upper())
+        batch = [f"<AA2fold> {spaced_seq}"]
 
         # tokenize sequences and pad up to the longest sequence in the batch
         ids = self.tokenizer.batch_encode_plus(batch, add_special_tokens=True, padding="longest",return_tensors='pt').to(self.device)
@@ -39,6 +38,8 @@ class ProstT5Embedding(Embedding):
 
         # if you want to derive a single representation (per-protein embedding) for the whole protein
         vector = emb_0.mean(dim=0) # shape (1024)
+
+        assert vector.shape == (1024,)
 
         if torch.isnan(vector).any():
             return None
