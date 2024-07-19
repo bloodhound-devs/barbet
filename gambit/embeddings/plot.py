09a4dad8d754883ba82c69ea2150adf1307e6a99
rturnbull <robert.turnbull@unimelb.edu.au>
2024-07-19 17:44:17 +1000

    :zap: fixing issues plotting embeddings

--- gambit/embeddings/plot.py
+++ gambit/embeddings/plot.py
@@ -6,21 +6,31 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import typer
+import torch
+import numpy as np
 
 
 app = typer.Typer()
 
-def plot_embeddings(seqtree:SeqTree, seqbank:SeqBank, gene_family_id:str, depth:int=1) -> go.Figure:
+def plot_embeddings(seqtree:SeqTree, seqbank:SeqBank, gene_family_id:str="", depth:int=1) -> go.Figure:
     categories = []
     data = []
-    for accession, details in seqtree.items():
-        if accession.endswith(f"/{gene_family_id}"):
-            node = details.node
-            ancestors = node.ancestors()
-            category = ancestors[depth].name
-            categories.append(category)
-            data.append(seqbank[accession])
+    for accession in seqtree:
+        if not gene_family_id:
+            gene_family_id = accession.split("/")[-1]
+            print(f"No gene family given. Using {gene_family_id}")
 
+        if accession.endswith(f"/{gene_family_id}"):
+            node = seqtree.node(accession)
+            category = node.ancestors[depth].name
+            categories.append(category)
+            embedding = seqbank[accession]
+            array = torch.frombuffer(embedding, dtype=torch.float32)
+            data.append(array.numpy())
+
+    data = np.array(data)
+    print(data.shape)
+    
     # Do PCA with sklearn
     pca = PCA(n_components=2)
     pca_data = pca.fit_transform(data)
@@ -28,13 +38,15 @@ def plot_embeddings(seqtree:SeqTree, seqbank:SeqBank, gene_family_id:str, depth:
     # Plot with plotly
     df = pd.DataFrame(pca_data, columns=["x", "y"])
     df["category"] = categories
-    fig = px.scatter(df, x="x", y="y", color="category")
+    fig = px.scatter(df, x="x", y="y", color="category", title=gene_family_id)
+    filename = f"{gene_family_id}.png"
+    print(f"Writing to {filename}")
     fig.write_image(f"{gene_family_id}.png")
     return fig
 
 
 @app.command()
-def plot_embeddings_cli(seqtree:Path, seqbank:Path, gene_family_id:str, depth:int=1, show:bool=False):
+def plot_embeddings_cli(seqtree:Path, seqbank:Path, gene_family_id:str="", depth:int=1, show:bool=False):
     seqtree = SeqTree.load(seqtree)
     seqbank = SeqBank(seqbank)
     fig = plot_embeddings(seqtree, seqbank, gene_family_id, depth)
