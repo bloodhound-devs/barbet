086e063946635c280eff54197ea66c196998d29d
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-10-29 08:56:54 +1100

    :zap: working on embedding long sequences

--- bloodhound/embedding.py
+++ bloodhound/embedding.py
@@ -11,6 +11,7 @@ import torch
 from io import StringIO
 from torchapp.cli import CLIApp, tool, method
 import typer
+from dataclasses import dataclass
 
 from .data import read_memmap, RANKS
 
@@ -58,15 +59,131 @@ def get_node(lineage:str, lineage_to_node:dict[str,SoftmaxNode]) -> SoftmaxNode:
     return node
 
 
+def generate_overlapping_intervals(total: int, interval_size: int, min_overlap: int, check:bool=True, variable_size:bool=False) -> list[tuple[int, int]]:
+    """
+    Creates a list of overlapping intervals within a specified range, adjusting the interval size to ensure
+    that the overlap is approximately the same across all intervals.
+
+    Args:
+        total (int): The total range within which intervals are to be created.
+        max_interval_size (int): The maximum size of each interval.
+        min_overlap (int): The minimum number of units by which consecutive intervals overlap.
+        check (bool): If True, checks are performed to ensure that the intervals meet the specified conditions.
+
+    Returns:
+        list[tuple[int, int]]: A list of tuples where each tuple represents the start (inclusive) 
+        and end (exclusive) of an interval.
+
+    Example:
+        >>> generate_overlapping_intervals(20, 5, 2)
+        [(0, 5), (3, 8), (6, 11), (9, 14), (12, 17), (15, 20)]
+    """
+    intervals = []
+    start = 0
+
+    if total == 0:
+        return intervals
+    
+    max_interval_size = interval_size
+    assert interval_size
+    assert min_overlap is not None
+    assert interval_size > min_overlap, f"Max interval size of {interval_size} must be greater than min overlap of {min_overlap}"
+
+    # Calculate the number of intervals needed to cover the range
+    num_intervals, remainder = divmod(total - min_overlap, interval_size - min_overlap)
+    if remainder > 0:
+        num_intervals += 1
+
+    # Calculate the exact interval size to ensure consistent overlap
+    overlap = min_overlap
+    if variable_size:
+        if num_intervals > 1:
+            interval_size, remainder = divmod(total + (num_intervals - 1) * overlap, num_intervals)
+            if remainder > 0:
+                interval_size += 1
+    else:
+        # If the size is fixed, then vary the overlap to keep it even
+        if num_intervals > 1:
+            overlap, remainder = divmod( num_intervals * interval_size - total, num_intervals - 1)
+            if overlap < min_overlap:
+                overlap = min_overlap
+
+    while True:
+        end = start + interval_size
+        if end > total:
+            end = total
+            start = max(end - interval_size,0)
+        intervals.append((start, end))
+        start += interval_size - overlap
+        if end >= total:
+            break
+
+    if check:
+        assert intervals[0][0] == 0
+        assert intervals[-1][1] == total
+        assert len(intervals) == num_intervals, f"Expected {num_intervals} intervals, got {len(intervals)}"
+
+        assert interval_size <= max_interval_size, f"Interval size of {interval_size} exceeds max interval size of {max_interval_size}"
+        for interval in intervals:
+            assert interval[1] - interval[0] == interval_size, f"Interval size of {interval[1] - interval[0]} is not the expected size {interval_size}"
+
+        for i in range(1, len(intervals)):
+            overlap = intervals[i - 1][1] - intervals[i][0]
+            assert overlap >= min_overlap, f"Min overlap condition of {min_overlap} not met for intervals {intervals[i - 1]} and {intervals[i]} (overlap {overlap})"
+
+    return intervals
+
+
+@dataclass
 class Embedding(CLIApp, ABC):
+    """ A class for embedding protein sequences. """
+    max_length:int|None=None
+    overlap:int=64
+
     @abstractmethod
     def embed(self, seq:str) -> torch.Tensor:
         """ Takes a protein sequence as a string and returns an embedding vector. """
-        pass
+        raise NotImplementedError
+
+    def reduce(self, tensor:torch.Tensor) -> torch.Tensor:
+        if tensor.ndims == 2:
+            tensor = tensor.mean(dim=0)
+        assert tensor.ndims == 1
+        return tensor
 
     def __call__(self, seq:str) -> torch.Tensor:
         """ Takes a protein sequence as a string and returns an embedding vector. """
-        return self.embed(seq)
+        if not self.max_length or len(seq) <= self.max_length:
+            tensor = self.embed(seq)
+            return self.reduce(tensor)
+        
+        epsilon = 0.1
+        intervals = generate_overlapping_intervals(len(seq), self.max_length, self.overlap)
+        weights = torch.zeros( (len(seq),) )
+        tensor = None
+        for start,end in intervals:
+            result = self.embed(seq[start:end])
+
+            assert result.shape[0] == end-start
+            embedding_size = result.shape[1]
+
+            if tensor is None:
+                tensor = torch.zeros( (len(seq), embedding_size ))
+
+            assert tensor.shape[-1] == embedding_size
+
+
+            interval_indexes = torch.arange(end-start)
+            distance_from_ends = torch.min( interval_indexes-start, end-interval_indexes  )
+            
+            weight = epsilon + distance_from_ends
+
+            tensor[start:end] += weight * result
+            weights[start:end] += weight
+
+        tensor = tensor/weight
+
+        return self.reduce(tensor)
     
     @method
     def setup(self, **kwargs):
